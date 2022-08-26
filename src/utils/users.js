9c4c220bb2c30fd635169e7142cbd6685140bf64
kakup07 <pandey.kaku@live.com>
2022-08-27 01:32:20 +0530

    rooms dropdown

--- src/utils/users.js
+++ src/utils/users.js
@@ -25,7 +25,7 @@ const addUser = ({ id, username, room }) => {
     //Store user
     const user = { id, username, room }
     users.push(user)
-    if (!(room in rooms)){
+    if (!(rooms.includes(room))){
         rooms.push(room)
     }
     return { user }
